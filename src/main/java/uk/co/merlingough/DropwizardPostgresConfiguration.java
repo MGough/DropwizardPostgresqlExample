53828fc6294be688dce25b6959c6860227b0c476
MGough <mbg317@bham.ac.uk>
2015-09-25 11:29:34 +0100

    Removed some redundant code leftover from HelloWorld example

--- src/main/java/uk/co/merlingough/DropwizardPostgresConfiguration.java
+++ src/main/java/uk/co/merlingough/DropwizardPostgresConfiguration.java
@@ -9,31 +9,6 @@ import javax.validation.Valid;
 import javax.validation.constraints.*;
 
 public class DropwizardPostgresConfiguration extends Configuration {
-    @NotEmpty
-    private String template;
-
-    @NotEmpty
-    private String defaultName = "Stranger";
-
-    @JsonProperty
-    public String getTemplate() {
-        return template;
-    }
-
-    @JsonProperty
-    public void setTemplate(String template) {
-        this.template = template;
-    }
-
-    @JsonProperty
-    public String getDefaultName() {
-        return defaultName;
-    }
-
-    @JsonProperty
-    public void setDefaultName(String name) {
-        this.defaultName = name;
-    }
 
     @Valid
     @NotNull
